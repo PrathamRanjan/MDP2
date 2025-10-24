c46e601249bce60cdcfbd956d4250b53f6cb268f
Tony Evans Adisurya <tonyevansadisurya@Tonys-MacBook-Pro.local>
2025-10-24 15:21:29 +0800

    Clean

--- STM32/Core/Inc/user_input.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_USER_INPUT_H_
-#define INC_USER_INPUT_H_
-
-#include "main.h"
-
-uint8_t user_is_pressed();
-
-#endif /* INC_USER_INPUT_H_ */
--- android/README.md
+++
@@ -1,3 +0,0 @@
-# Android
-Version: API 30 - Android 11.0 (R),
-Support Galaxy Tab A7 Lite
